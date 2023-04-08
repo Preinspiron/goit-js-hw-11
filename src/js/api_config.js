6b3d28b5c2f89186e05a6501285acf0765a2a422
V_Borysenko <preinspiron@gmail.com>
2023-04-09 01:54:56 +0200

    111

--- src/js/api_config.js
+++ src/js/api_config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = '35160864-8ca81e651f434a7e9dfe21623';
 
-export default fetchPhotos = axios.create({
+export default axios.create({
   baseURL: 'https://pixabay.com/api/',
   params: {
     key: API_KEY,
